837536ff8dbd766c8637b3e1cf82a4a6e7c90d9d
Jorge Hernandez <jorge_l_hernandez@yahoo.com>
2017-07-13 06:06:05 -0400

    Started working on the cloud component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import Cloud from './Components/Cloud/Cloud';
+
 class App extends Component {
-  render() {
+    render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Cloud />
       </div>
     );
-  }
+  } 
 }
 
 export default App;
---
+++ src/Components/Cloud/Cloud.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Cloud.css';
+
+class Cloud extends Component {
+    render() {
+        return (
+            <div className="Cloud"></div>
+        );
+    } 
+}
+
+export default Cloud;

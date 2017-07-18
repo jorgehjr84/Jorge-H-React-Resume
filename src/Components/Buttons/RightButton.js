f62dfb55ad6877c2fbf2c63a237d733368a63a74
Jorge Hernandez <jorge_l_hernandez@yahoo.com>
2017-07-18 05:34:00 -0400

    Re adding the deploy script

---
+++ src/Components/Buttons/RightButton.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './button.css';
+
+class RightButton extends Component {   
+    render() {
+        
+        return (
+            <div className="RightButton"></div>
+        );
+    } 
+}
+
+export default RightButton;
